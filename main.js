472776a6add5a4ae5c92b7cc0f585d63571a750f
lloydpop <lloydperfect199@gmail.com>
2023-09-30 14:28:17 +0100

    updated

---
+++ main.js
@@ -0,0 +1,24 @@
+// Get references to DOM elements
+const body = document.querySelector("body"),
+  hourHand = document.querySelector(".hour"),
+  minuteHand = document.querySelector(".minute"),
+  secondHand = document.querySelector(".second"),
+  soundButton = document.querySelector(".play-sound");
+
+const updateTime = () => {
+  // Get current time and calculate degrees for clock hands
+  let date = new Date(),
+    secToDeg = (date.getSeconds() / 60) * 360,
+    minToDeg = (date.getMinutes() / 60) * 360,
+    hrToDeg = (date.getHours() / 12) * 360;
+
+  // Rotate the clock hands to the appropriate degree based on the current time
+  secondHand.style.transform = `rotate(${secToDeg}deg)`;
+  minuteHand.style.transform = `rotate(${minToDeg}deg)`;
+  hourHand.style.transform = `rotate(${hrToDeg}deg)`;
+};
+//call function when dom is loaded
+document.addEventListener("DOMContentLoaded", function () {
+  setInterval(updateTime, 1000);
+  updateTime();
+});
